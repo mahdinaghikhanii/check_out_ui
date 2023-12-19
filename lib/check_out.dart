47440d7407f9e4204737932a8492c924fa0684f3
Mahdinkh7 <mahdinkh7@gmail.com>
2023-12-19 20:45:06 +0330

    fist

---
+++ lib/check_out.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CheckOutPages extends StatelessWidget {
+  const CheckOutPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
